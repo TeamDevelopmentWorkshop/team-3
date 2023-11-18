0c74c7bc6eed1cce54fb2c8c9f6877ec7a27f3df
ikuta2023-takahashi-koutarou <ce222042@meiji.ac.jp>
2023-11-18 16:24:09 +0900

    さらに渦巻きをしてる

--- enemy.py
+++ enemy.py
@@ -4,7 +4,6 @@ ENEMY_WIDTH = 8
 ENEMY_HEIGHT = 8
 ENEMY_SPEED = 1.5
 
-
 class Enemy:
     def __init__(self, x, y):
         self.x = x
@@ -14,22 +13,26 @@ class Enemy:
         self.dir = 1
         self.timer_offset = pyxel.rndi(0, 59)
         self.is_alive = True
-
+       
+        
     def update(self):
         # y方向には一定の速度(ENEMY_SPEED)で移動
-
         # x方向の速度は一定の間隔で向きを変える
         # ヒント１：frame_count変数を適当な値で割った時の余りを使って条件分岐する
         # ヒント２：frame_countに乱数（timer_offset）を加えて、向きを変えるタイミングをランダムにする
-        #self.y += ENEMY_SPEED
-        self.y = pyxel.frame_count * math.sin(ENEMY_SPEED * pyxel.frame_count) #y速度の計算(渦巻きver)
-        if(((pyxel.frame_count + self.timer_offset) % 60) < 30): #分岐の閾値は30に設定
-            self.dir = 1 #右方向に進行
+        number = 1#pyxel.rndi(0,1)
+        if(number == 0):
+            self.y += ENEMY_SPEED #y速度
+            self.x  = self.x + self.dir * ENEMY_SPEED #x速度の計算
+
+
+        elif(number == 1):
+            self.y = pyxel.frame_count/500 + 50 + math.exp(pyxel.frame_count/200) * math.sin(ENEMY_SPEED * pyxel.frame_count * math.pi/50) #y速度の計算(渦巻きver)
+            self.x = pyxel.frame_count/500 + 50 + math.exp(pyxel.frame_count/200) * math.cos(ENEMY_SPEED * pyxel.frame_count * math.pi/50) #x速度の計算(渦巻きver)
         else:
-            self.dir = -1 #30以上の時に左方向に進行
-        
-        #self.x  = self.x + self.dir * ENEMY_SPEED #x速度の計算
-        self.x = pyxel.frame_count * math.cos(ENEMY_SPEED * pyxel.frame_count) #x速度の計算(渦巻きver)
+            self.y += ENEMY_SPEED #y速度
+            self.x  = self.x + self.dir * ENEMY_SPEED #x速度の計算
+
         if(self.y > pyxel.height):
             self.is_alive = False
         else:
