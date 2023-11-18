82817fe99d22e1d23d0961f24bb1390fa9f42e0d
ikuta2023-takahashi-koutarou <ce222042@meiji.ac.jp>
2023-11-18 15:21:59 +0900

    効果音の追加

--- blast.py
+++ blast.py
@@ -16,10 +16,16 @@ class Blast:
         # 自身の半径を1だけ大きくする
         # 閾値（BLAST_END_RADIUS）より半径が大きくなったら、is_aliveをFalseにする
         self.radius += 1
+        pyxel.tick = 0
+        pyxel.loop = True
+        pyxel.sound(0).set("a1a-2a3 rr b1b#2b3 rr","p t","6 7","v f",25)
+        pyxel.play(0,0,pyxel.tick,pyxel.loop)
         if(self.radius > BLAST_END_RADIUS):
             self.is_alive = False
+            pyxel.stop(0)
         else:
             self.is_alive = True
+        
 
         return
 
@@ -29,12 +35,11 @@ class Blast:
         pyxel.circ(self.x,self.y,self.radius,BLAST_COLOR_IN)
         pyxel.circb(self.x,self.y,self.radius,BLAST_COLOR_OUT)
         return
-
+    
     def test(self):
         pyxel.init(120, 160, title="Pyxel Shooter example")
         pyxel.run(self.update, self.draw)
-
-
+    
 if __name__ == '__main__':
     blast_test = Blast(10, 20)
     Blast.test(blast_test)
